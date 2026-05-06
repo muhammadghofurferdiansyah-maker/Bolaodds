ef5361ff0203efc24c8e8535dcc3e2db18e9d776
muhammadghofurferdiansyah-maker <muhammadghofurferdiansyah@gmail.com>
2026-05-07 05:23:59 +0700

    Update Inden.js

--- Inden.js
+++ Inden.js
@@ -1,1 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+const token = '8799312893:AAHd3DVeYlyanyMHHmLd50nDvxHhk262kOo';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id,
+`🔥 Selamat datang di GhofurPred Bot
+
+Bot prediksi sepak bola ⚽
+
+Ketik:
+/prediksi`);
+});
+
+bot.onText(/\/prediksi/, (msg) => {
+
+    const data = [
+        "Man City vs Arsenal = OVER 2.5",
+        "Real Madrid vs Sevilla = BTTS YES",
+        "Inter vs Milan = OVER CORNER"
+    ];
+
+    bot.sendMessage(msg.chat.id,
+`📊 PREDIKSI HARI INI
+
+${data.join("\n")}
+
+🔥 Monte Carlo AI`);
+});
+
+console.log("Bot aktif");
